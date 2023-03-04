9614516f05199a09d70fe01de280bdef55d24db0
abhayfaldu <abhayfaldu1922@gmail.com>
2023-03-04 21:31:04 +0530

    added multiplayer and chat

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-const cors = require("cors");
 const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
 const connectDB = require("./configs/db.js");
 require("dotenv").config();
 const { userRouter } = require("./routes/User.route.js");
-
-const app = express();
-app.use(express.json());
+const { Server } = require("socket.io");
 app.use(cors());
 
-// app.get("/", (req, res) => {
-// 	res.send("Welcome to home page!");
-// });
+const server = http.createServer(app);
+
+const io = new Server(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"],
+	},
+});
+
+io.on("connection", socket => {
+	const userID = socket.id;
+	console.log("connected to " + userID);
+
+	socket.on("join_room", (room) => { 
+		socket.join(room);
+		console.log(`User with ID: ${userID} joined room ${room}`);
+	})
+
+	socket.on("send_message", (messageData) => { 
+		// console.log('messageData:', messageData)
+		socket.to(messageData.room).emit("receive_message", messageData);
+	})
+
+	socket.on("player_played", boardState => {
+		// console.log("boardState:", boardState);
+		socket
+			.to(boardState.room)
+			.emit("player_played_server_to_client", boardState);
+	});
+
+	socket.on("disconnect", () => { 
+		console.log("disconnected from " + userID);
+	})
+});
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+	res.send("Welcome to home page!");
+});
 
 app.use("/user", userRouter);
 
-app.listen(process.env.PORT, async () => {
+server.listen(3001, async () => {
 	try {
 		await connectDB;
 		console.log("DB connected");
 	} catch (error) {
 		console.log(error);
 	}
-	console.log(
-		`Your server is listening to http://localhost:${process.env.PORT}`
-	);
+	console.log("SERVER RUNNING");
 });
